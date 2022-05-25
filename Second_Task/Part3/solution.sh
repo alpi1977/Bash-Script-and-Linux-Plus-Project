d4425a2c4e73b24d212bf8647560507f74ddef7e
alpi1977 <aozkan1977@gmail.com>
2022-05-25 16:53:25 +0300

    Part3 files added

---
+++ Second_Task/Part3/solution.sh
@@ -0,0 +1,5 @@
+grep "Invalid user" auth.log | cut -d' ' -f8 | uniq -c > invalid_user.sh
+
+grep "Invalid user" auth.log | cut -d' ' -f9 | uniq -c >> invalid_user.sh
+
+cat invalid_user.sh | grep -v "from" | grep -v "user" > new_invalid_user.txt
